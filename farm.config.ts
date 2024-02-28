c083b4aa923f206873a2df65f2b95fc2fbebac9c
Ben Hohner <benhohner@gmail.com>
2024-02-28 02:00:24 -0500

    minfity

--- farm.config.ts
+++ farm.config.ts
@@ -3,7 +3,8 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   compilation: {
-    sourcemap: 'all-inline'
+    sourcemap: 'all-inline',
+    minify: false,
   },
   vitePlugins: [
     vue(),
